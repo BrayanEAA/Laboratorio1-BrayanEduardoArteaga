4f548cbf01ae4cc33f38ad572e585f8daf7c733d
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-02-28 21:49:45 -0600

    fin del programa

---
+++ laboratorio1/lab/Negocio/ClsPersona.cs
@@ -0,0 +1,45 @@
+﻿using lab.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab.Negocio 
+{
+   
+    
+        public class ClsPersona
+        {
+            public static string CalcularIMC(Persona persona)
+            {
+                double imc = persona.Peso / (persona.Altura * persona.Altura);
+                if (imc < 24)
+                {
+                    return "Peso ideal";
+                }
+                else
+                {
+                    return "Tiene sobrepeso";
+                }
+            }
+
+            public static string EsMayorDeEdad(Persona persona)
+            {
+                if (persona.Edad >= 18)
+                {
+                    return "Sí";
+                }
+                else
+                {
+                    return "No";
+                }
+            }
+        }
+    }
+
+
+
+
